b4b01239fc77dd6bfd761fc3ccc18311d50087a0
Stadnichenko Oleg <oleg.stadnichenko@gmail.com>
2017-01-07 15:30:31 +0300

    забыл добавить в комит Barrel distortion shader

---
+++ res/raw/fss4.glsl
@@ -0,0 +1,45 @@
+// Barrel distortion
+//#version 120 error on adnroid 5 with this line
+uniform sampler2D sTexture;
+varying vec2 texCoord;
+const float PI = 3.1415926535;
+//uniform float BarrelPower;
+uniform vec2 uCenter;
+uniform vec2 uCenter2;
+uniform vec2 uRadius;
+
+vec2 Distort(vec2 p)
+{
+    float theta  = atan(p.y, p.x);
+    float radius = length(p);
+    float BarrelPower = 2.0;
+    radius = pow(radius, BarrelPower);
+    p.x = radius * cos(theta);
+    p.y = radius * sin(theta);
+    //return 0.5 * (p + 1.0);
+    return p;
+}
+
+void main()
+{
+  vec2 uv = texCoord.xy;
+
+  vec2 xy = (texCoord.xy - uCenter) * 10.0;
+  float d = length(xy);
+  if (d < 1.0)
+  {
+    uv = Distort(xy);
+    uv = uv / 10.0;
+    uv = uv + uCenter;
+  }
+  xy = (texCoord.xy - uCenter2) * 10.0;
+  d = length(xy);
+  if (d < 1.0)
+  {
+    uv = Distort(xy);
+    uv = uv / 10.0;
+    uv = uv + uCenter2;
+  }
+  vec4 c = texture2D(sTexture, uv);
+  gl_FragColor = c;
+}
---
+++ src/ru/flightlabs/masks/utils/CameraHelper.java
@@ -0,0 +1,14 @@
+package ru.flightlabs.masks.utils;
+
+/**
+ * Created by sov on 07.01.2017.
+ */
+
+public class CameraHelper {
+
+
+    public static void findGoodResolution(int width, int height, float deviation) {
+        // TODO find minimum resolution (x * y) with minimum deviation of x and y
+
+    }
+}
